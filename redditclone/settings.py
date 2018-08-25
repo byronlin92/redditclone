12b5e2c07154eafae86e85d7bbd8d164aa47133b
Byron Lin <byronlin92@gmail.com>
2018-08-25 00:38:39 -0700

    a

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['redditclone92.herokuapp.com']
 
 
 # Application definition
