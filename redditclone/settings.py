3b606073ce4e4da3c33b7a8727ddb8d78fbf3206
Byron Lin <byronlin92@gmail.com>
2018-08-24 20:49:46 -0700

    make it better

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -141,3 +141,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+django_heroku.settings(locals())
